0cccf53913edcbf9947969087fb710ac94f0a2fd
Samuel N. ~Desktop <samuelnarciso3280@gmail.com>
2021-03-28 17:48:36 -0600

    Coloca el numero de lineas, coloca un fondo degradado a lineas pares"

--- static/js/index.js
+++ static/js/index.js
@@ -2,35 +2,72 @@ const textAreaInput = document.querySelector('#textAreaInput');
 const mostrar_consola = document.querySelector('#mostrar_consola');
 const log = document.querySelector('#log');
 const comprobar = document.querySelector('#comprobar');
+const identador = document.querySelector('.identador');
 
 mostrar_consola.addEventListener('click', () => {
     log.classList.toggle('hideBox');
-    textAreaInput.classList.toggle('reduce50');
+    // textAreaInput.classList.toggle('reduce50');
 });
 
 comprobar.addEventListener('click', () => {
-    log.innerHTML = " ";
-    const cadena = {'cadena': textAreaInput.innerText};
-    const jcadena = JSON.stringify(cadena)
-    var url = '/';
+    // log.innerHTML = " ";
+    // const cadena = {'cadena': textAreaInput.innerText};
+    // const jcadena = JSON.stringify(cadena)
+    // var url = '/';
 
-    fetch(url, {
-        method: 'POST',
-        body: jcadena,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(resp => resp.json()).then(data => {
-        for (var i in data['mensajes']) {
-            var error = data['mensajes'][i];
-            const label = document.createElement('label');
-            label.classList.add('textLog');
-            label.innerText = error;
-            if (error.charAt(0) == 'E') {
-                label.classList.add('error')
+    // fetch(url, {
+    //     method: 'POST',
+    //     body: jcadena,
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(resp => resp.json()).then(data => {
+    //     for (var i in data['mensajes']) {
+    //         var error = data['mensajes'][i];
+    //         const label = document.createElement('label');
+    //         label.classList.add('textLog');
+    //         label.innerText = error;
+    //         if (error.charAt(0) == 'E') {
+    //             label.classList.add('error')
+    //         }
+    //         log.append(label);
+
+    //     }
+    // })
+})
+
+
+
+const identacion = (childs) => {
+
+    const linesIdentador = (cantidad) => {
+        // console.log(cantidad);
+        identador.innerHTML = '';
+        for (let i = 1; i <= cantidad+1; i++) {
+            if (i < 10) {
+                identador.append('0' + i);
+            } else {
+                identador.append(i);
+
             }
-            log.append(label);
+
 
         }
-    })
+
+    }
+
+    childs.forEach(child => {
+        if (child.classList) {
+            child.classList.add('lineBoxShape');
+            linesIdentador(childs.length);
+        }
+    });
+
+}
+
+textAreaInput.addEventListener('keydown', (e) => {
+    // console.log(e);
+    if (e.key === 'Enter' || e.key === 'Backspace' || e.key === 'Delete' ) {
+        identacion(textAreaInput.childNodes)
+    }
 })
