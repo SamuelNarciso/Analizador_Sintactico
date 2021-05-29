f8785bc051081a765c4c78c73723f9498a50abe6
dlowTux <omarlopez22520@gmail.com>
2021-05-29 17:27:41 -0500

    Fix the bugs with the many lines into a while

--- AnalizadorSintactico/While.py
+++ AnalizadorSintactico/While.py
@@ -13,24 +13,30 @@ class While:
             if 'while' in x:
                 found = True
             if found:
-                whileaux += x
+                whileaux += x+';'
                 position.append(count)
             if '}' in x:
                 found = False
                 # Get the thre parts of the while
                 w = []
+                waux=[]
                 if '(' in whileaux and ')' in whileaux:
                     w.append(whileaux[whileaux.index(
                         '(')+1:whileaux.index(')')])
                 if '{' in whileaux and '}' in whileaux:
-                    w.append(whileaux[whileaux.index(
+                    waux.append(whileaux[whileaux.index(
                         '{')+1:whileaux.index('}')])
                 if len(w) < 2:
                     errores.append('While error de sistaxis')
                 else:
                     errores.append('While  correcto')
                 del self.string[position[0]:position[-1]+1]
+                seperar=waux[0].split(';')
+                for y in seperar:
+                    if y!='':
+                        w.append(y)
                 for h in w:
                     self.string.append(h)
             count += 1
+        print(self.string)
         return self.string, errores
