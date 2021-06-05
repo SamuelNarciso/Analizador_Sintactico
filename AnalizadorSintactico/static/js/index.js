2a9b030fe3023ef90bd5c28149c72317a3e879b3
dlowTux <omarlopez22520@gmail.com>
2021-06-05 10:44:13 -0500

    Add the full proyect

--- AnalizadorSintactico/static/js/index.js
+++ AnalizadorSintactico/static/js/index.js
@@ -85,136 +85,31 @@ comprobar.addEventListener("click", () => {
         })
         .then(() => {
             if (bien) {
-                let url;
+                let texto = [];
                 const aux = textAreaInput.innerText.split("\n");
-                aux.forEach((element) => {
-                    if (element) {
-                        resultado = IsOperation(element);
-                        let opc = ["", ""];
-                        if (resultado) {
-                            opc = element.split("=");
-                        }
-                        let operacion = "";
-                        for (let i in opc[1]) {
-                            if (opc[1].charAt(i) === "/") {
-                                operacion += "_";
-                            } else {
-                                operacion += opc[1].charAt(i);
-                            }
-                        }
-                        url = "http://localhost:3000/" + operacion;
-                        fetch(url)
-                            .then((x) => x.json())
-                            .then((data) => {
-                                if (
-                                    data.operacion.output_array != null &&
-                                    opc[0] != ""
-                                ) {
-                                    let oper = data.operacion.output_array;
-                                    console.log(oper)
-                                    console.log(oper.length)
-                                    if (oper.length === 0) {
-                                        let pila = IsOperationString(element)
-                                        pila.push(opc[0])
-                                        pila.push('=')
-
-                                        //Separar por el = y ver si es una suma de string
-                                        code.push({
-                                            original: element,
-                                            traduccion: {
-                                                asigna: pila
-                                            },
-                                        });
-                                    } else {
-                                        oper.push(opc[0]);
-                                        oper.push("=");
-
-                                        code.push({
-                                            original: element,
-                                            traduccion: {asigna: oper},
-                                        });
-                                    }
-                                }
-                            })
-
-                            .then(() => {
-                                const resultado = IsVar(element, {
-                                    expresion: /int/,
-                                    tipo: "int",
-                                    valor: 0,
-                                });
-                                if (resultado != false) {
-                                    code.push({
-                                        original: element,
-                                        traduccion: resultado,
-                                    });
-                                }
-                            })
-                            .then(() => {
-                                const resultado = IsVar(element, {
-                                    expresion: /float/,
-                                    tipo: "float",
-                                    valor: 0.0,
-                                });
-                                if (resultado != false) {
-                                    code.push({
-                                        original: element,
-                                        traduccion: resultado,
-                                    });
-                                }
-                            })
-                            .then(() => {
-                                const resultado = IsVar(element, {
-                                    expresion: /String/,
-                                    tipo: "String",
-                                    valor: "",
-                                });
-                                if (resultado != false) {
-                                    code.push({
-                                        original: element,
-                                        traduccion: resultado,
-                                    });
-                                }
-                            })
-
-                            .then(() => {
-                                if ((resultado = IsWrite(element))) {
-                                    const auxcadena = element.split("Write(");
-                                    const mensaje = auxcadena[1].substring(
-                                        0,
-                                        auxcadena[1].length - 1
-                                    );
-
-                                    code.push({
-                                        original: element,
-                                        traduccion: {imprime: mensaje},
-                                    });
-                                }
-                            })
-                            .then(() => {
-                                if (aux[aux.length - 1] === element) {
-                                    const ordenada = textAreaInput.innerText.split(
-                                        "\n"
-                                    );
-                                    ordenada.forEach((objeto) => {
-                                        code.forEach((traductor) => {
-                                            if (traductor.original === objeto) {
-                                                traduccion.push(
-                                                    traductor.traduccion
-                                                );
-                                            }
-                                        });
-                                    });
-                                    console.log(
-                                        JSON.stringify({codigoP: traduccion})
-                                    );
-                                    insertar_codigoP(
-                                        JSON.stringify({codigoP: traduccion})
-                                    );
-                                }
-                            });
+                let i = 0;
+                while (i < aux.length) {
+                    iswhile = IsWhile(aux[i]);
+                    if (iswhile === true) {
+                        //Aqui es donde esta lo del while en una sola linea
+                        resultado = Search(i, aux);
+                        resultado.cadena.forEach((simbolo) =>
+                            texto.push(simbolo)
+                        );
+                        i = resultado.aumento;
+                    } else {
+                        //Aqui solo insertamos los elementos a la pila
+                        texto.push(aux[i]);
+                        i++;
                     }
-                });
+                }
+                console.log(texto);
+                return texto;
+            }
+        })
+        .then((texto) => {
+            if (bien) {
+                Promesas(texto);
             }
         });
 });
@@ -232,3 +127,189 @@ textAreaInput.addEventListener("keydown", (e) => {
         identacion(textAreaInput.childNodes);
     }
 });
+
+function Search(count, matrix) {
+    aux = "";
+    pila = [];
+    let valor = 0;
+
+    for (let i = count; i < matrix.length; i++) {
+        aux += matrix[i] + ";";
+        if (matrix[i] === "}") {
+            valor = i;
+            break;
+        }
+    }
+
+    valor++;
+    pila = AcomodarWhile(aux);
+    return {cadena: pila, aumento: valor};
+}
+function AcomodarWhile(texto) {
+    let aux = texto.split(";");
+    let resultado = [];
+    aux.forEach((e) => {
+        if (e) {
+            if (IsWhile(e)) {
+                let tabla = e.split("while(");
+                let t2 = tabla[1].split("){");
+                resultado.push("while " + t2[0]);
+                resultado.push("{");
+            } else {
+                resultado.push(e);
+            }
+        }
+    });
+    return resultado;
+}
+function IsWhile(elemento) {
+    const patter = /while/;
+    return elemento.match(patter) != null;
+}
+function Promesas(texto) {
+    let url;
+    const aux = texto;
+    aux.forEach((element) => {
+
+        if (element) {
+            resultado = IsOperation(element);
+            let opc = ["", ""];
+            if (resultado) {
+                opc = element.split("=");
+            }
+            let operacion = "";
+            for (let i in opc[1]) {
+                if (opc[1].charAt(i) === "/") {
+                    operacion += "_";
+                } else {
+                    operacion += opc[1].charAt(i);
+                }
+            }
+            url = "http://localhost:3000/" + operacion;
+            fetch(url)
+                .then((x) => x.json())
+                .then((data) => {
+                    if (data.operacion.output_array != null && opc[0] != "") {
+                        let oper = data.operacion.output_array;
+                        if (oper.length === 0) {
+                            let pila = IsOperationString(element);
+                            pila.push(opc[0]);
+                            pila.push("=");
+
+                            //Separar por el = y ver si es una suma de string
+                            code.push({
+                                original: element,
+                                traduccion: {
+                                    asigna: pila,
+                                },
+                            });
+                        } else {
+                            oper.push(opc[0]);
+                            oper.push("=");
+
+                            code.push({
+                                original: element,
+                                traduccion: {asigna: oper},
+                            });
+                        }
+                    }
+                })
+
+                .then(() => {
+                    const resultado = IsVar(element, {
+                        expresion: /int/,
+                        tipo: "int",
+                        valor: 0,
+                    });
+                    if (resultado != false) {
+                        code.push({
+                            original: element,
+                            traduccion: resultado,
+                        });
+                    }
+                })
+                .then(() => {
+                    const resultado = IsVar(element, {
+                        expresion: /float/,
+                        tipo: "float",
+                        valor: 0.0,
+                    });
+                    if (resultado != false) {
+                        code.push({
+                            original: element,
+                            traduccion: resultado,
+                        });
+                    }
+                })
+                .then(() => {
+                    const resultado = IsVar(element, {
+                        expresion: /String/,
+                        tipo: "String",
+                        valor: "",
+                    });
+                    if (resultado != false) {
+                        code.push({
+                            original: element,
+                            traduccion: resultado,
+                        });
+                    }
+                })
+
+                .then(() => {
+                    if ((resultado = IsWrite(element))) {
+                        const auxcadena = element.split("Write(");
+                        const mensaje = auxcadena[1].substring(
+                            0,
+                            auxcadena[1].length - 1
+                        );
+
+                        code.push({
+                            original: element,
+                            traduccion: {imprime: mensaje},
+                        });
+                    }
+                })
+                .then(() => {
+                    if (IsWhile(element)) {
+                        const condicion = element.split(" ");
+                        code.push({
+                            original: element,
+                            traduccion: {mientras: condicion[1]}
+                        });
+                    }
+                }).then(() => {
+                    if (element === '{') {
+                        code.push({
+                            original: element,
+                            traduccion: {inicio: 'inicio'}
+                        })
+                    }
+                }).then(() => {
+                    if (element === '}') {
+                        code.push({
+                            original: element,
+                            traduccion: {fin: 'fin'}
+                        })
+                    }
+                })
+
+                .then(() => {
+                    if (aux[aux.length - 1] === element) {
+                        for (let i = 0; i < texto.length; i++) {
+                            for (let j = 0; j < code.length; j++) {
+                                if (code[j].original == texto[i]) {
+                                    traduccion.push(code[j].traduccion)
+                                    break;
+                                }
+                            }
+                        }
+
+                        console.log(JSON.stringify({codigoP: traduccion}));
+                        insertar_codigoP(
+                            JSON.stringify({codigoP: traduccion})
+                        );
+                    }
+                });
+        }
+    });
+}
